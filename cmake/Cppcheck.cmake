9ba65e50fedd970c50c32dc24810437b16f05100
Mohammad Rahimi <32928433+MhmRhm@users.noreply.github.com>
2025-02-11 10:58:08 +0800

    Support inline suppression in Cppcheck
    Added --inline-suppr switch to Cppcheck.

--- cmake/Cppcheck.cmake
+++ cmake/Cppcheck.cmake
@@ -2,6 +2,6 @@ function(AddCppcheck target)
 	find_program(CPPCHECK_PATH cppcheck REQUIRED PATHS "${CPPCHECK_INSTALL_DIR}")
 	set_target_properties(${target}
 		PROPERTIES CXX_CPPCHECK
-		"${CPPCHECK_PATH};--enable=warning;--error-exitcode=10"
+		"${CPPCHECK_PATH};--enable=warning;--error-exitcode=10;--inline-suppr"
 	)
 endfunction()
