a98d82aa7f35d2d1ca22a30079cb2332693d6f50
Mohammad Rahimi <rahimi.mhmmd@gmail.com>
2025-10-25 06:17:05 +0000

    Improve Boost.Test integration

--- cmake/Testing.cmake
+++ cmake/Testing.cmake
@@ -3,10 +3,11 @@ enable_testing()
 find_package(GTest CONFIG REQUIRED)
 
 include(GoogleTest)
+include(Boost)
 include(Coverage)
 include(Memcheck)
 
-macro(AddTests target)
+macro(AddGTests target)
 	AddCoverage(${target})
 	target_link_libraries(${target}
 		PRIVATE GTest::gtest
@@ -15,5 +16,12 @@ macro(AddTests target)
 		PRIVATE GTest::gmock_main
 	)
 	gtest_discover_tests(${target})
+	AddMemcheck(${target})
+endmacro()
+
+macro(AddBoostTests target)
+	AddCoverage(${target})
+	target_link_libraries(${target} PRIVATE Boost::unit_test_framework)
+	add_test(NAME "${target}" COMMAND ${target}) # All tests run as one
 	AddMemcheck(${target})
 endmacro()
